d91a80553e6fef16b52b64abccde30726b86b368
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-06-26 13:36:59 +0300

    Init project

---
+++ UkrainiansId/UkrainiansId.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using UkrainiansId.Domain.Models;
+namespace UkrainiansId.Domain.Interfaces
+{
+    public interface IRepository<T> where T : BaseModel
+    {
+        Task<T> CreateAsync(T entity);
+        Task<List<T>> CreateListAsync(List<T> entities);
+        Task<T> UpdateAsync(T entity);
+        Task<List<T>> UpdateListAsync(List<T> entities);
+        Task<T> DeleteAsync(T entity);
+        Task<List<T>> DeleteListAsync(List<T> entities);
+        Task<List<T>> GetAllAsync(bool all = false, int skip = 0, int take = 20, bool isTracking = false);
+        Task<List<T>> GetListAsync(Expression<Func<T, bool>> match, bool isTracking = false);
+        Task<int> CountAsync(Expression<Func<T, bool>> match = null);
+        Task<long> CountLongAsync(Expression<Func<T, bool>> match = null);
+        Task<bool> IsExistAsync(Expression<Func<T, bool>> match);
+        Task<int> SaveChangesAysnc();
+        Task<IQueryable<T>> FromSqlAsync(string sql, params object[] parameters);
+    }
+}
---
+++ UkrainiansId/UkrainiansId.Domain/Models/BaseModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UkrainiansId.Domain.Models
+{
+    public class BaseCreateModel
+    {
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string CreatedFromIP { get; set; }
+    }
+
+    public class BaseUpdateModel : BaseCreateModel
+    {
+        public DateTime? LastUpdatedAt { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public string LastUpdatedFromIP { get; set; }
+    }
+
+    public class BaseModel : BaseUpdateModel
+    {
+
+    }
+
+    public class BaseModel<Tid> : BaseModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Tid Id { get; set; }
+    }
+    public class BaseModelWithoutGen<Tid> : BaseModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Tid Id { get; set; }
+    }
+}
