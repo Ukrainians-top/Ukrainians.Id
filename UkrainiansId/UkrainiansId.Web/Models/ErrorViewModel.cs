e67d28f912d659b94c650116b42d240ac28cb644
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-06-26 18:18:48 +0300

    Added MVC project

---
+++ UkrainiansId/UkrainiansId.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UkrainiansId.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- UkrainiansId/UkrainiansId.Web/Startup.cs
+++ UkrainiansId/UkrainiansId.Web/Startup.cs
@@ -1,39 +1,97 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace UkrainiansId.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
+            _configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration _configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "App";
+            })
+            .AddAmazon(options =>
+            {
+                options.ClientId = _configuration["Keys:Amazon:ClientId"];
+                options.ClientSecret = _configuration["Keys:Amazon:ClientSecret"];
+            })
+            .AddApple(options =>
+            {
+                options.ClientId = _configuration["Keys:Apple:ClientId"];
+                options.ClientSecret = _configuration["Keys:Apple:ClientSecret"];
+            })
+            .AddDiscord(options =>
+            {
+                options.ClientId = _configuration["Keys:Discord:ClientId"];
+                options.ClientSecret = _configuration["Keys:Discord:ClientSecret"];
+            })
+            .AddFacebook(options =>
+            {
+                options.ClientId = _configuration["Keys:Facebook:ClientId"];
+                options.ClientSecret = _configuration["Keys:Facebook:ClientSecret"];
+            })
+            .AddGoogle(options =>
+            {
+                options.ClientId = _configuration["Keys:Google:ClientId"];
+                options.ClientSecret = _configuration["Keys:Google:ClientSecret"];
+            })
+            .AddInstagram(options =>
+            {
+                options.ClientId = _configuration["Keys:Instagram:ClientId"];
+                options.ClientSecret = _configuration["Keys:Instagram:ClientSecret"];
+            })
+            .AddLinkedIn(options =>
+            {
+                options.ClientId = _configuration["Keys:LinkedIn:ClientId"];
+                options.ClientSecret = _configuration["Keys:LinkedIn:ClientSecret"];
+            })
+            .AddSpotify(options =>
+            {
+                options.ClientId = _configuration["Keys:Spotify:ClientId"];
+                options.ClientSecret = _configuration["Keys:Spotify:ClientSecret"];
+            })
+            .AddTwitch(options =>
+            {
+                options.ClientId = _configuration["Keys:Twitch:ClientId"];
+                options.ClientSecret = _configuration["Keys:Twitch:ClientSecret"];
+            });
+
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
